207cc23b8eac8608921695136916537e596606a4
marcoguazzini <79971842+marcoguazzini@users.noreply.github.com>
2022-09-10 11:06:41 +0200

    Update plot.R

--- R/plot.R
+++ R/plot.R
@@ -40,14 +40,15 @@ plot <- function(counts, comp_counts, chan_name){
 plot_grid(
  ggplot(tb_counts_combined, aes_string(x = ch1, y =ch2)) +
     geom_hex(bins = 64) +
+   ggtitle("Untransformed data")+
     coord_fixed() +
     colorscale +
     facet_wrap(~comp),
   
  ggplot(tb_counts_combined_transformed, aes_string(x = ch1, y = ch2)) +
     geom_hex(bins = 64) +
+   ggtitle("Transformed data with asinh(x/5) transformation")+
     coord_fixed() +
     colorscale +
     facet_wrap(~comp))
- 
 }
