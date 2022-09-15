e7c7232041663916e313b1aefad9e5c03413d9aa
Marco Guazzini <79971842+marcoguazzini@users.noreply.github.com>
2022-09-15 13:33:54 +0200

    Update prepare_data.R

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -1,10 +1,30 @@
+#' Generate synthetic data according to the hierarchical model
+#'
+#' @export
+#'
+#' @param spillover_matrix Square matrix of spillover values
+#'
+#' @param n_cells Number of iterations
+#' 
+#' @param rates Gamma distribution rates
+#' 
+#' @param shapes Gamma distribution rates
+#'
+#' @param n_cells Number of iterations
+#' 
+#' @return matrix
+#'
+#' @examples
+#' set.seed(23)
+#' sm <- spillR::load_spillover()
+#' shapes <- 10
+#' rates <- 1
+#' n_cells <- 5000
+#' counts <- prepare_data(sm, shapes, rates, n_cells)
+
+
 prepare_data <- function(spillover_matrix,shape,rate, n_cells){
-    #' Generate synthetic data according to the hierarchical model
-    #' Args:
-    #'     spillover_matrix: square matrix of spillover values
-    #'     n_sample: number of iterations
-    #'     rates: gamma distribution rates
-    #'     shapes: gamma distribution shapes
+ 
     n <- ncol(spillover_matrix)
     channel_names <- rownames(spillover_matrix)
     lambdas = matrix(rgamma(n*n_cells, shape = shape, rate = rate),
