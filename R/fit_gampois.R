502c8168fecd81f46baf8c6eab01a0df65216892
Marco Guazzini <79971842+marcoguazzini@users.noreply.github.com>
2022-09-27 11:55:35 +0200

    Update fit_gampois.R

--- R/fit_gampois.R
+++ R/fit_gampois.R
@@ -23,25 +23,72 @@
 #' fit <- fit_gampois(x = x, offset = offset, starting_value = c(1,0.1))
 #' fit
 fit_gampois <- function(x,
-                        offset = 0,
-                        starting_value = c(1,0.1)) {
-  library("extraDistr")
-  library("matrixStats")
-  dgpoisshifted <- function(y, r, b, t, log = FALSE) {
-    pm <- 0
-    if(!log) {
-      factor1 <- exp(-t)*b^r / (factorial(y)*gamma(r))
-      term <- function(k) choose(y, k)*t^(y-k)*gamma(k+r)/(b+1)^(k+r)
-      factor2 <- sum(sapply(0:y, term))
-      pm <- factor1 * factor2
-    } else {
-      factor1 <- -t+log(b)*r-(lfactorial(y)+lgamma(r))
-      term <- function(k) lchoose(y,k)+(y-k)*log(t)+ lgamma(k+r)-(k+r)*log(b+1)
+                       offset = 0,
+                       starting_value = c(1,0.1,0)) {
+  library(matrixStats)
+  dgpoisshifted_zeroinf <- function(y, r, b, p, t, log = FALSE) {
+  pm <- 0
+  if(!log) {
+    factor1 <- exp(-t) * b^r / (factorial(y) * gamma(r))
+    term <- function(k) choose(y, k) * t^(y-k) * gamma(k+r) / (b+1)^(k+r)
+    factor2 <- sum(sapply(0:y, term))
+    pm <- p*(y == 0)+ (1 - p)*( factor1*factor2)
+  } else {
+     if( y != 0 || p == 0){
+    factor1 <- -t + r*log(b) - (lfactorial(y) + lgamma(r))
+    term <- function(k) lchoose(y, k) + (y-k)*log(t) + lgamma(k+r) - (k+r)*log(b+1)
+    factor2 <- 0
+    if(t > 0) {
       factor2 <- logSumExp(sapply(0:y, term))
-      pm <- factor1 + factor2
+    } else {
+      factor2 <- lgamma(y+r) - (y+r)*log(b+1)
     }
-    pm
+    pm <- log(1-p)+factor1+factor2
+    }
+     else{
+      la <- log(p)
+      lb <- log(1-p)-t+r*log(b)-r*log(b+1)
+      pm <- logSumExp(c(la,lb))
+    }
   }
+  pm
+}
+  
+  likelihood <- function(param,x){
+     shape <- param[1]
+    rate <- param[2]
+    p <- param[3]
+    ll <- dgpoisshifted_zeroinf(x, shape, rate, p, t = offset, log=TRUE)
+    return(ll)
+  }
+  log_lik <- function(param, x) {
+    sumll <- -sum(sapply(x, function(xi)
+      likelihood(param, xi)))
+    return(sumll)
+  }
+  res <- optim(
+    starting_value,
+    log_lik ,
+    x = x,
+    method = "L-BFGS-B",
+    lower = c(1,0.001, 0) # to be fixed 
+  )$par
+  
+  data.frame(shape_hat = res[1], 
+             rate_hat = res[2],
+             p_hat = res[3]) 
+}
+  res <- optim(
+    starting_value,
+    log_lik ,
+    x = x,
+    method = "L-BFGS-B",
+    lower = c(1, 0.001)
+  )$par
+  
+  data.frame(shape_hat = res[1], 
+             rate_hat = res[2]) 
+}
   likelihood <- function(param,x){
     shape <- param[1]
     rate <- param[2]
