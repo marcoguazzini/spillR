422b2ae9f5cfa9cb8157746b0ad022fe5442b737
Marco Guazzini <79971842+marcoguazzini@users.noreply.github.com>
2022-09-29 12:28:07 +0200

    Updated zero inflation

--- R/fit_gampois.R
+++ R/fit_gampois.R
@@ -22,43 +22,54 @@
 #' x <- rpois(n_sample, lambda = lambda + offset)
 #' fit <- fit_gampois(x = x, offset = offset, starting_value = c(1,0.1))
 #' fit
+
 fit_gampois <- function(x,
-                       offset = 0,
-                       starting_value = c(1, 0.1, 0.1)) {
-  library(matrixStats)
+                        offset = 0,
+                        starting_value = c(1, 0.1, 0)) {
   dgpoisshifted_zeroinf <- function(y, r, b, p, t, log = FALSE) {
-  pm <- 0
-  if(!log) {
-    factor1 <- exp(-t) * b^r / (factorial(y) * gamma(r))
-    term <- function(k) choose(y, k) * t^(y-k) * gamma(k+r) / (b+1)^(k+r)
-    factor2 <- sum(sapply(0:y, term))
-    pm <- p*(y == 0)+ (1 - p)*( factor1*factor2)
-  } else {
-     if( y != 0 || p == 0){
-    factor1 <- -t + r*log(b) - (lfactorial(y) + lgamma(r))
-    term <- function(k) lchoose(y, k) + (y-k)*log(t) + lgamma(k+r) - (k+r)*log(b+1)
-    factor2 <- 0
-    if(t > 0) {
-      factor2 <- logSumExp(sapply(0:y, term))
+    
+    if (!log) {
+      factor1 <- exp(-t) * b ^ r / (factorial(y) * gamma(r))
+      term <-
+        function(k)
+          choose(y, k) * t ^ (y - k) * gamma(k + r) / (b + 1) ^ (k + r)
+      factor2 <- sum(sapply(0:y, term))
+      return(p * (y == 0) + (1 - p) * (factor1 * factor2))
     } else {
-      factor2 <- lgamma(y+r) - (y+r)*log(b+1)
-    }
-    pm <- log(1-p)+factor1+factor2
-    }
-     else{
-      la <- log(p)
-      lb <- log(1-p)-t+r*log(b)-r*log(b+1)
-      pm <- logSumExp(c(la,lb))
+      if (y > 0) {
+        factor1 <- -t + r * log(b) - (lfactorial(y) + lgamma(r))
+        term <-
+          function(k)
+            lchoose(y, k) + (y - k) * log(t) + lgamma(k + r) - (k + r) * log(b + 1)
+        factor2 <- 0
+        if (t > 0) {
+          factor2 <- logSumExp(sapply(0:y, term))
+        } else {
+          factor2 <- lgamma(y + r) - (y + r) * log(b + 1)
+        }
+        # will produce -Inf if p = 1, 
+        # but p = 1 means that there are only zeros,
+        # we exclude 1 by setting the limit of p to 1 - epsilon
+        return(log(1 - p) + factor1 + factor2)
+      }
+      else {
+        lb <- log(1 - p) - t + r * log(b) - r * log(b + 1)
+        if(p == 0) {
+          lx <- lb
+        } else {
+          la <- log(p)
+          lx <- logSumExp(c(la, lb))
+        }
+        return(lx)
+      }
     }
   }
-  pm
-}
   
-  likelihood <- function(param,x){
-     shape <- param[1]
+  likelihood <- function(param, x) {
+    shape <- param[1]
     rate <- param[2]
     p <- param[3]
-    ll <- dgpoisshifted_zeroinf(x, shape, rate, p, t = offset, log=TRUE)
+    ll <- dgpoisshifted_zeroinf(x, shape, rate, p, t = offset, log = TRUE)
     return(ll)
   }
   log_lik <- function(param, x) {
@@ -68,13 +79,14 @@ fit_gampois <- function(x,
   }
   res <- optim(
     starting_value,
-    log_lik ,
+    log_lik,
     x = x,
     method = "L-BFGS-B",
-    lower = c(0.1, 0.001, 0) # to be fixed 
+    lower = c(.Machine$double.eps, .Machine$double.eps, 0),
+    upper = c(Inf, Inf, 1-.Machine$double.eps)
   )$par
   
-  data.frame(shape_hat = res[1], 
+  data.frame(shape_hat = res[1],
              rate_hat = res[2],
-             p_hat = res[3]) 
+             p_hat = res[3])
 }
