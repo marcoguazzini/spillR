9d50ec0cf2684ec19ce4d8acc9fe0ce248b83735
Marco Guazzini <79971842+marcoguazzini@users.noreply.github.com>
2023-01-23 16:32:24 +0100

    Update mixture_alg.R

--- R/mixture_alg.R
+++ R/mixture_alg.R
@@ -1,5 +1,5 @@
 mixture_alg <- function(counts, smc, thr = 0.995, cut = FALSE, n_degree = 4){
-colnames(counts) -> channel_names
+channel_names <- colnames(counts) 
 targets <- unname(channel_names[-(1:4)])
 barcode_targets <- unname(sapply(targets , function(t) substr(t, 3,5)))
 marker_selections <- sapply(targets, function(t) list(names(smc[, t])[smc[, t] > 0]))
