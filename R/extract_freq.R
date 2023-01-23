191f39ca5a2068e85e4e76f4166399effb12ff57
Marco Guazzini <79971842+marcoguazzini@users.noreply.github.com>
2023-01-23 17:15:23 +0100

    Update extract_freq.R

--- R/extract_freq.R
+++ R/extract_freq.R
@@ -1,4 +1,4 @@
-extract_freq <- function(target, counts, thr = 0.995, cut = FALSE){
+extract_freq <- function(target, counts, threshold = 0.995, cut = FALSE){
   if(!cut){
 x <- counts[,target]
 tb_freq <- tibble(x) %>% group_by(x) %>% tally()
@@ -7,7 +7,7 @@ tb_freq
 }
   else{
     x <- counts[,target]
-    x <- outlier_cut(x, p = thr )
+    x <- outlier_cut(x, p = threshold )
     tb_freq <- tibble(x) %>% group_by(x) %>% tally()
     tb_freq$x %<>% as.integer()
     tb_freq
