a634b3b7322c89234834a97d3c9a36cb24535620
Marco Guazzini <79971842+marcoguazzini@users.noreply.github.com>
2023-02-06 14:23:46 +0100

    Update marker_pois_regression.R

--- R/marker_pois_regression.R
+++ R/marker_pois_regression.R
@@ -2,11 +2,10 @@ marker_pois_regression <-
   function(target,
            barcode_target,
            barcode_emit,
-           counts_spill,
           n_degree,
           smc) {
     
-    tb_freq <- emit_extraction(target, barcode_target, barcode_emit, counts_spill, smc)
+    tb_freq <- emit_extraction(target, barcode_target, barcode_emit,  smc)
     # need to make sure to use raw = TRUE
     fit <- glm(
       n ~ poly(x, degree = n_degree, raw = TRUE),
