212fe6d9457d8632c6103fa40df5c63e364e11a8
Marco Guazzini <79971842+marcoguazzini@users.noreply.github.com>
2023-02-08 11:23:09 +0100

    Update offset_comp.R

--- R/offset_comp.R
+++ R/offset_comp.R
@@ -1,5 +1,5 @@
 offset_comp <- function(tb_freq,betas,target, barcode_target, barcode_emit, counts_spill,smc){
-  tb <- emit_extraction(target, barcode_target, barcode_emit, counts_spill,smc)
+  tb <- emit_extraction(target, barcode_target, barcode_emit,smc)
   offset <- function(x,betas,tb){
     if(x <= max(range(tb$x)) && x >= min(range(tb$x))){
       xp <- c(1, matrix(poly(x, degree = n_degree, raw = TRUE)))
