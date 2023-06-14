f9cafb6fdda681b1ff92053790ddadc96cf13daf
Marco Guazzini <79971842+marcoguazzini@users.noreply.github.com>
2023-06-14 14:34:54 +0200

    Add files via upload

---
+++ R/generate_real.R
@@ -0,0 +1,25 @@
+#' Generate dataset for vignettes and simulation studies
+#'
+#' @import tibble
+#' @export
+#'
+#' @return \code{\link[tibble]{tibble}} data frame
+#'
+#' @examples
+#' set.seed(23)
+#' df <- generate_real()
+#' df
+generate_real <- function(){
+  n_real <-10000
+  lambda_real <- 100
+  lambda_bead  <- 70
+  spill_prob   <- 0.1
+  # real experiment
+  Z_target <- rpois(n = n_real, lambda = lambda_real)
+  Z_spill  <- rpois(n = n_real, lambda = lambda_bead)
+  spill    <- rbinom(n = n_real, size = 1, prob = spill_prob)
+  Y        <- (1-spill)*Z_target + spill*Z_spill
+  df_real  <- tibble(Y = Y, barcode = "Y", type = "real cells")
+  df_real
+
+}
